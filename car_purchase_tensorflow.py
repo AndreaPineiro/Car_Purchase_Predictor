68ea0c0a5255dd9327abf43db5475a6f4d57dd94
AndreaPineiro <58478323+AndreaPineiro@users.noreply.github.com>
2022-09-11 21:46:36 -0500

    Add files via upload

--- car_purchase_tensorflow.py
+++ car_purchase_tensorflow.py
@@ -40,20 +40,25 @@ model.compile(
 # Entrenar el modelo
 print('MODELO 1')
 print('Inicio del entrenamiento')
-historia = model.fit(x_train, y_train, epochs = 150, verbose = 2)
-print("Modelo 1 entrenado")
+historia = model.fit(x_train, y_train, epochs = 100, verbose = True, validation_split = 0.2)
+print("Modelo entrenado")
 
 # Graficar la pérdida
 plt.subplot(1, 3, 1)
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-plt.plot(historia.history["loss"])
+plt.plot(historia.history["loss"], label='Train Loss')
+plt.plot(historia.history["val_loss"], label='Validation Loss')
+plt.legend()
 
 # Graficar el accuracy
 plt.subplot(1, 3, 2)
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
-plt.plot(historia.history["binary_accuracy"], color = "green")
+plt.plot(historia.history["binary_accuracy"], color = "green", label='Train Accuracy')
+plt.plot(historia.history["val_binary_accuracy"], label='Validation Accuracy')
+
+plt.legend()
 plt.show()
 
 # Predicciones
@@ -82,7 +87,7 @@ model2.compile(
 # Entrenar el modelo
 print('MODELO 2')
 print('Inicio del entrenamiento')
-historia = model2.fit(X_trans, Y, epochs = 300, validation_split = 0.2, verbose = 2)
+historia = model2.fit(X_trans, Y, epochs = 400, validation_split = 0.2, verbose = 2)
 print("Modelo entrenado")
 
 # Graficar la pérdida
@@ -90,14 +95,14 @@ plt.subplot(1, 3, 1)
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.plot(historia.history["loss"], label='Train Loss')
-plt.plot(historia.history["val_loss"], label='Test Loss')
+plt.plot(historia.history["val_loss"], label='Validation Loss')
 
 # Graficar el accuracy
 plt.subplot(1, 3, 2)
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
 plt.plot(historia.history["binary_accuracy"], color = "green", label='Train Accuracy')
-plt.plot(historia.history["val_binary_accuracy"], label='Test Accuracy')
+plt.plot(historia.history["val_binary_accuracy"], label='Validation Accuracy')
 
 plt.legend()
 plt.show()
@@ -106,4 +111,4 @@ plt.show()
 y_hat=model2.predict(x_test)
 y_hat=[0 if val<0.5 else 1 for val in y_hat]
 print("Roc AUC Modelo 2: ", roc_auc_score(y_test,y_hat))
-print("Binary Accuracy Modelo 2: ", accuracy_score(y_test,y_hat))
+print("Test Accuracy Modelo 2: ", accuracy_score(y_test,y_hat))
